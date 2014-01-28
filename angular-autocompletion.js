901c1e3cb63ac01eba24f7244d8fd30ff61c6357
Andreas Larsson <larsson_andreas@live.se>
2014-01-28 16:56:27 +0100

    Added directive

---
+++ angular-autocompletion.js
@@ -0,0 +1,26 @@
+angular.module('autocompletion', []).directive('autocompletion', function($timeout){
+    return{
+        restrict: 'AC',
+        scope:{
+            ngModel: '=',
+            onComplete: '&',
+            wait: '=?',
+            min: '=?'
+        },
+        require: 'ngModel',
+        link: function(scope, element){
+            scope.wait = scope.wait || 300; //wait default to 300ms
+            scope.min = scope.min || 2; //at least 2 chars before calling oncomplete
+            
+            var timeout;
+            scope.$watch('ngModel', function(newVal, oldVal){
+                if(!newVal || newVal.length < scope.min || newVal == oldVal) return;
+                
+                $timeout.cancel(timeout);
+                timeout = $timeout(function(){
+                    scope.onComplete();
+                }, scope.wait);
+            });
+        }
+    };
+});
